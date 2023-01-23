891a5c0998a598fae343782b400b5cdac56703a5
Praful Maka <57310445+prafulmaka@users.noreply.github.com>
2023-01-22 20:32:08 -0800

    01/22/2023 - Working on fractional_knapsack

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,16 +1,59 @@
 from sys import stdin
 
-data = list(map(int, input().split()))
-print("Data Original: ", data)
-n, capacity = data[0:2]
-print("Capacity: ", capacity)
-values = data[2:(2 * n + 2):2]
-print("Values", values)
-weights = data[3:(2 * n + 2):2]
-print("Weights: ", weights)
+
+def optimal_value(capacity, weights, values):
+
+    def get_ratio(values, weights):
 
 
-# # Original
+
+    value = 0
+    # write your code here
+
+    # Test: 3 50 60 20 100 50 120 30
+    # Test: 2 30 100 20 50 50
+
+    print("Capacity: ", capacity)
+    print("Weights: ", weights)
+    print("Value: ", values)
+
+    # To find most expensive and index of most expensive
+    # print("Max: ", max(values))
+    # print("Index: ", values.index(max(values)))
+
+    while (capacity > 0):
+        # Set most expensive, index of most expensive
+        most_expensive = max(values)
+        ix_most_expensive = values.index(max(values))
+
+        # If capacity is less than or equal to the weight of most expensive
+        if capacity <= weights[ix_most_expensive]:
+            print("True")
+            value = value + (capacity * most_expensive)
+            capacity = 0
+            print(capacity)
+
+        # If capacity is more than the weight of most expensive
+        if capacity > weights[ix_most_expensive]:
+            print("False")
+            value = value + (capacity * most_expensive)
+            capacity = capacity - weights[ix_most_expensive]
+            values.remove(values[ix_most_expensive])
+            weights.remove(weights[ix_most_expensive])
+            print(capacity)
+
+    return value
+
+
+if __name__ == "__main__":
+    data = list(map(int, stdin.readline().split()))
+    n, capacity = data[0:2]
+    values = data[2:(2 * n + 2):2]
+    weights = data[3:(2 * n + 2):2]
+    opt_value = optimal_value(capacity, weights, values)
+    print("{:.10f}".format(opt_value))
+
+# # # Original
 # from sys import stdin
 #
 #
@@ -22,7 +65,7 @@ print("Weights: ", weights)
 #
 #
 # if __name__ == "__main__":
-#     data = list(map(int, stdin.read().split()))
+#     data = list(map(int, stdin.readline().split()))
 #     n, capacity = data[0:2]
 #     values = data[2:(2 * n + 2):2]
 #     weights = data[3:(2 * n + 2):2]
